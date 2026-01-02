ad503905002256129adee46e3d0b5cd4359e8e23
Muhammad Bin Arif <m.binarif.work@gmail.com>
2026-01-02 17:29:01 +0500

    challenge_1 Final

--- Challenge1_Team_Contri_Multi.py
+++ Challenge1_Team_Contri_Multi.py
@@ -1,29 +1,18 @@
-def team_contribution(contribution):
-    left = []
-    right = []
-    impact = []
-    for i in range(len(contribution)):
-        if i == 0 :
-            left.append(1)
-        left_temp = 1
-        if i != 0 :
-            for j in range(i):
-                left_temp *= contribution[j]
-            left.append(left_temp)
-    for i in range(len(contribution)):
-        if i == len(contribution) - 1:
-            right.append(1)
-        right_temp = 1
-        if i != len(contribution) - 1:
-            for j in range(i+1,len(contribution)):
-                right_temp *= contribution[j]
-            right.append(right_temp)
+def team_contribution(contribution):    
+    left = [1] * len(contribution)
+    right = [1] * len(contribution)
+    impact = [1] * len(contribution)
 
-    for i in range(len(left)):
-        for j in range(len(right)):
-            if i == j:
-                impact.append(left[i]*right[j])
+    for i in range(1, len(contribution)):
+        left[i] = left[i-1] * contribution[i-1]
+
+    for i in range(len(contribution)-2, -1, -1):
+        right[i] = right[i+1] * contribution[i+1]
+
+    for i in range(len(contribution)):
+        impact[i] = left[i] * right[i]
+
     print(f"Contribution Array: {contribution}")
     print(f"Impact Array: {impact}")
 
-team_contribution([1,2,3,4])
+team_contribution([3,5,2,7])
